05cd240ea5bd65edd1029abcf4ece93e2b4ce6d4
Nehemias Munoz <nehemiasmunozm@gmail.com>
2023-10-23 18:29:37 -0300

    Initial

---
+++ src/main/java/com/marvic/centro_dental/CentroDentalApplication.java
@@ -0,0 +1,13 @@
+package com.marvic.centro_dental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentroDentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CentroDentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/marvic/centro_dental/CentroDentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvic.centro_dental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentroDentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
